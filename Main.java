abb375f2308e5ff13f2643ad5d2610be2a161309
jrondina <jamesrondina@Gmail.com>
2016-06-29 05:57:00 -0400

    jrondina interfaces and abstract lab

---
+++ Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Remote> remotes = new ArrayList<>();
+        remotes.add(new Samsung(true, false));
+        remotes.add(new Sony(false, true));
+        remotes.add(new Sharp(false, false));
+
+        for (Remote remote : remotes)  {
+            remote.numberButtonPressed(7);
+            remote.channelUp();
+            remote.channelDown();
+            remote.volumeUp();
+            remote.volumeDown();
+
+        }
+    }
+}
